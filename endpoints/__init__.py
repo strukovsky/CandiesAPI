534fde126de4498101c1d17804a0f2db8705df35
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-05-15 20:01:41 +0300

    chore: create routing

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,5 +1,17 @@
+import os
+
 from fastapi import FastAPI
 from db import Session
 
 app = FastAPI()
 session = Session()
+HOST = os.environ.get("HOST", "http://127.0.0.1")
+PORT = os.environ.get("PORT", "8000")
+
+
+@app.get("/")
+def root():
+    return {
+        "users": f"{HOST}:{PORT}/users/",
+        "chupachupses": f"{HOST}:{PORT}/chupachupses/",
+    }
