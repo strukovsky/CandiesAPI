05101247dba7b674048cfd20950ea6f87fbdd8ef
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-05-15 19:11:47 +0300

    feat: add user endpoints

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from db import User, Session
+from models import UserModel
 
 app = FastAPI()
+session = Session()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/users/")
+async def get_all_users():
+    return {
+        "users": session.query(User).all()}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/users/{username}/")
+async def get_specific_user(username: str):
+    return {username: session.query(User).filter(User.username == username).first()}
+
+
+@app.post("/users/")
+async def create_new_user(user: UserModel):
+    user_to_db = User(**user.dict())
+    session.add(user_to_db)
+    session.commit()
