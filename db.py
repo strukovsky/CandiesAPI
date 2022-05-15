5f8175788bcc0e66a06710d5c7cc20d45b911b69
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-05-15 19:11:40 +0300

    feat: add user model

---
+++ db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, String, create_engine, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///database.db")
+Base = declarative_base(bind=engine)
+Session = sessionmaker(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, unique=True, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True)
+
+
+Base.metadata.create_all()
